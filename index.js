9481aac4e776859ca040867e7b444442ac367a93
oanhpham <phamoanh22365@gmail.com>
2023-02-03 13:21:11 +0700

    twelve

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 let myLibrary = [];
-let form = document.getElementById("form-new-book");
-
-function displayForm(){
-    form.style.display = 'block';
-}
+let formAddBook = document.getElementById("form-new-book");
+let listBook =  document.getElementById("library-content");
 
 class Book {
     constructor(title, author, pages, read) {
@@ -14,13 +11,20 @@ class Book {
     }
 }
 
+function displayForm(){
+    formAddBook.style.display = 'block';
+    listBook.style.display = "none";
+}
+
 function addBookToLibrary() {
     event.preventDefault();
-    form.style.display = 'none';
-    var title = document.getElementById("nameBook").value; 
-    var author = document.getElementById("author").value; 
-    var pages = document.getElementById("pages").value; 
-    var read = document.getElementById("stateRead").checked; 
+    formAddBook.style.display = 'none';
+    listBook.style.display = "block";
+
+    let title = document.getElementById("nameBook").value; 
+    let author = document.getElementById("author").value; 
+    let pages = document.getElementById("pages").value; 
+    let read = document.getElementById("stateRead").checked; 
     newBook = new Book(title, author, pages,read);
     myLibrary.push(newBook); 
     document.getElementById("form").reset();
@@ -33,50 +37,52 @@ function createCard(newBook){
     li.setAttribute("class","card")
 
     const heading = document.createElement('h3');
-    heading.innerHTML="Book";
+    heading.innerHTML = "Book";
 
     const titleDiv = document.createElement('div');
     titleDiv.setAttribute("class","nameBook");
-    titleDiv.innerHTML= "Name: " + newBook.title;
+    titleDiv.innerHTML = "Name: " + newBook.title;
 
     const authDiv = document.createElement('div');
     authDiv.setAttribute("class","author");
-    authDiv.innerHTML= "Author: " + newBook.author;
+    authDiv.innerHTML = "Author: " + newBook.author;
 
     const pageDiv = document.createElement('div');
     pageDiv.setAttribute("class","pages");
-    pageDiv.innerText= "Pages: " + newBook.pages;
+    pageDiv.innerText = "Pages: " + newBook.pages;
 
     const readBtn = document.createElement('button');
     readBtn.setAttribute("class","stateRead");
-    if(newBook.read == "1"){
-        readBtn.innerText= "Readed";
+    
+    if(newBook.read === "true"){
+        readBtn.innerText = "Readed";
         readBtn.style.backgroundColor="#f43f5e";
     }else{
-        readBtn.innerText= "Read";
+        readBtn.innerText = "Read";
         readBtn.style.backgroundColor="#22c55e";
     }
+
     readBtn.addEventListener("click",function(){
-       if(newBook.read=="true"){
-        readBtn.innerText= "Read";
-        readBtn.style.backgroundColor="#22c55e";
-        newBook.read="false";
+       if(newBook.read === "true"){
+        readBtn.innerText = "Read";
+        readBtn.style.backgroundColor = "#22c55e";
+        newBook.read = "false";
        }else{
-        readBtn.innerText= "Readed";
-        readBtn.style.backgroundColor="#f43f5e";
-        newBook.read="true";
+        readBtn.innerText = "Readed";
+        readBtn.style.backgroundColor = "#f43f5e";
+        newBook.read = "true";
        }
     });
 
-
     const removeBtn = document.createElement('button');
     removeBtn.setAttribute("class","removeBook");
-    removeBtn.innerHTML= "Remove book";
+    removeBtn.innerHTML = "Remove book";
+
     removeBtn.addEventListener("click",function(){
-        for (let i=0;i<myLibrary.length;i++){
-            if(myLibrary[i]==newBook){
+        for (let i = 0; i < myLibrary.length; i++){
+            if(myLibrary[i] === newBook){
                 let bookRemove = document.getElementsByClassName("card")[i];
-                bookRemove.style.display="none";
+                bookRemove.remove();
             }
         }
         myLibrary.splice(myLibrary.indexOf(newBook),1);
